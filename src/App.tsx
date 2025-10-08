8f5ded753b307e908df0e92205bb2dabc1f73be1
urkodi <damilyolivo@gmail.com>
2025-10-07 21:57:19 -0400

    Last commit, mistake

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,19 @@
-import Main from "./features/Main";
-import Sidenav from "./features/Sidenav";
-import UserPanel from "./features/UserPanel";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+import Panels from "./features/Panels";
 import CreateAccountPage from "./pages/CreateAccountPage";
 import LoginPage from "./pages/LoginPage";
+import Homepage from "./pages/Homepage";
 
 
 function App() {
   return(
     <Router>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <Sidenav />
-              <Main />
-              <UserPanel />
-            </>
-          }
-        />  
+        <Route path="/" element={<Panels />} />  
         <Route path="/create-account" element={<CreateAccountPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/homepage" element={<Homepage />} />
         //add more routes here as needed
       </Routes>
     </Router>
