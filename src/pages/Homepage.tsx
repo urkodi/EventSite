4543e2b42f5c5bb851fe8b7c0db6f6b250282096
urkodi <damilyolivo@gmail.com>
2025-10-15 15:14:01 -0400

    Some edits + working on homepage

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,7 +1,5 @@
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
 
+import EventBlock from "../components/EventBlock";
 import Panels from "../features/Panels";
 
 
@@ -9,12 +7,11 @@ function Homepage() {
     return (
         <>
             <Panels>
-                <header>
-                    <h1> Hello </h1>
-                </header>
-                <search>
-                    
-                </search>
+                <ul className="flex m-4 gap-4">
+                    <EventBlock eventId="1" imageUrl="https://photos.zillowstatic.com/fp/42667657f3aee8e431f9d37624c1fdac-p_d.webp" link="https://cornhub.website" eventTitle="Bop w/ Bops" eventDate="October 30th 2025" eventAddress="44 Hummingbird Ln" /> 
+                    <EventBlock eventId="1" imageUrl="https://photos.zillowstatic.com/fp/42667657f3aee8e431f9d37624c1fdac-p_d.webp" link="https://cornhub.website" eventTitle="Bop w/ Bops" eventDate="October 30th 2025" eventAddress="44 Hummingbird Ln" /> 
+                    <EventBlock eventId="1" imageUrl="https://photos.zillowstatic.com/fp/42667657f3aee8e431f9d37624c1fdac-p_d.webp" link="https://cornhub.website" eventTitle="Bop w/ Bops" eventDate="October 30th 2025" eventAddress="44 Hummingbird Ln" /> 
+                </ul>
             </Panels>
         </>
     )
