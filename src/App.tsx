a5bcab7db51b469f49777ec6f950bf97a7e7a191
Analiz Rodriguez <analiz.rodriguez74@gmail.com>
2025-10-20 14:27:01 -0400

    new changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from "./features/Main";
 import Sidenav from "./features/Sidenav";
 import UserPanel from "./features/UserPanel";
-
+import EventCheckout from "./eventCheckout";
 
 function App() {
-  return(
-    <>
-      <Sidenav />
-      <Main/>
-      <UserPanel/>  
-    </>
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Home page  */}
+        <Route path="/" element={
+          <>
+            <Sidenav />
+            <Main/>
+            <UserPanel/>
+          </>
+        } />
+        
+        {/* Checkout page */}
+        <Route path="/checkout" element={<EventCheckout />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
