56a9902c13b0b36ee54772b8938d12e4992bd898
FatimaTolo <fatoumatoutolo@gmail.com>
2025-10-22 16:20:42 -0400

    app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
-import Main from "./features/Main";
-import Sidenav from "./features/Sidenav";
-import UserPanel from "./features/UserPanel";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Panels from "./features/Panels";
+import CreateAccountPage from "./pages/CreateAccountPage";
+import LoginPage from "./pages/LoginPage";
+import HelpPage from "./pages/HelpPage";
+import ContactUsPage from "./pages/ContactUsPage";
+import Homepage from "./pages/Homepage";
+import MyTickets from "./pages/MyTickets";
+import Bookmark from "./pages/Bookmark";
 
 
 function App() {
   return(
-    <>
-      <Sidenav />
-      <Main/>
-      <UserPanel/>  
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Panels />} />  
+        <Route path="/create-account" element={<CreateAccountPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/help" element={<HelpPage />} />
+        <Route path="/contact-us" element={<ContactUsPage />} />
+        <Route path="/homepage" element={<Homepage />} />
+        <Route path="/mytickets" element={<MyTickets />} />
+        <Route path="/bookmark" element={<Bookmark />} />
+      </Routes>
+    </Router>
   )
 }
 
