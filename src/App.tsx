eb45c5406ba66f0e0fe7ae7b9cbf6ba6c53f43f3
christinagm <chrissygm9@gmail.com>
2025-10-30 20:08:09 -0400

    added checkout router

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import ContactUsPage from "./pages/ContactUsPage";
 import Homepage from "./pages/Homepage";
 
 import ForgotPassword from "./pages/ForgotPassword";
+import EventCheckout from "./EventCheckout";
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
         <Route path="/help" element={<HelpPage />} />
         <Route path="/contact-us" element={<ContactUsPage />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/checkout" element={<EventCheckout />} />
       </Routes>
     </Router>
   )
